50b146f467d66cd12b4214f552bbb243d5192746
Mathilde Lannes <mathilde.lannes@icm-institute.org>
2023-03-29 18:05:26 +0200

    Add social links

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled, {ThemeProvider} from "styled-components";
 import {GlobalStyle, theme} from "../styles";
 import Nav from "./Nav";
+import Footer from "./Footer";
+import Head from "./Head";
+import Loader from "./icons/loader";
+import Social from "./Social";
+import Email from "./Email";
 
 const StyledContent = styled.div`
   display: flex;
@@ -11,10 +16,43 @@ const StyledContent = styled.div`
 
 const Layout = ({ children, location }) => {
     const isHome = location.pathname === '/';
+    const [isLoading, setIsLoading] = useState(isHome);
+
+    // Sets target="_blank" rel="noopener noreferrer" on external links
+    const handleExternalLinks = () => {
+        const allLinks = Array.from(document.querySelectorAll('a'));
+        if (allLinks.length > 0) {
+            allLinks.forEach(link => {
+                if (link.host !== window.location.host) {
+                    link.setAttribute('rel', 'noopener noreferrer');
+                    link.setAttribute('target', '_blank');
+                }
+            });
+        }
+    };
+
+    useEffect(() => {
+        if (isLoading) {
+            return;
+        }
+
+        if (location.hash) {
+            const id = location.hash.substring(1); // location.hash without the '#'
+            setTimeout(() => {
+                const el = document.getElementById(id);
+                if (el) {
+                    el.scrollIntoView();
+                    el.focus();
+                }
+            }, 0);
+        }
+
+        handleExternalLinks();
+    }, [isLoading]);
 
     return (
         <div>
-            {/* TODO add Head */}
+            <Head location={location} />
             <div id="root">
                 <ThemeProvider theme={theme}>
                     <GlobalStyle />
@@ -25,10 +63,12 @@ const Layout = ({ children, location }) => {
 
                     <StyledContent>
                         <Nav isHome={isHome} />
+                        <Social isHome={isHome} />
+                        <Email isHome={isHome} />
 
                         <div id="content">
                             {children}
-                            {/*<Footer />*/}
+                            <Footer />
                         </div>
                     </StyledContent>
                 </ThemeProvider>

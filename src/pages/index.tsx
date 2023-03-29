247bdf5bb58b97ded23589ed88b54081771f6620
Mathilde Lannes <mathilde.lannes@icm-institute.org>
2023-03-29 15:05:19 +0200

    Add hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import Layout from "../components/Layout";
+import Hero from "../components/sections/Hero";
 
 const IndexPage: React.FC<PageProps> = ({ location }) => {
   return (
     <Layout location={location}>
-      <div style={{ marginTop: 100}}>hi there</div>
+      <Hero />
     </Layout>
   )
 }

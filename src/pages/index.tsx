f1f69d48bf5718d4b710e170a5a68ef2e67a4a21
Mathilde Lannes <mathilde.lannes@icm-institute.org>
2023-03-29 15:12:07 +0200

    Add "about me" section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import Layout from "../components/Layout";
 import Hero from "../components/sections/Hero";
+import About from "../components/sections/About";
 
 const IndexPage: React.FC<PageProps> = ({ location }) => {
   return (
     <Layout location={location}>
-      <Hero />
+        <Hero />
+        <About />
     </Layout>
   )
 }
